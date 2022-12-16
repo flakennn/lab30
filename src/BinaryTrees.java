86f98fa3eb1f33d6dfb57c6101e1fc9c5be06628
flakennn <qwer2323vex@yandex.ru>
2022-12-16 19:27:02 +0300

    123

--- src/BinaryTrees.java
+++ src/BinaryTrees.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class BinaryTrees {
+public class BinaryTrees {
+    private BinaryTrees left;
+    private BinaryTrees right;
+    private int value;
+    private static BinaryTrees root = null;
+
+    public BinaryTrees(BinaryTrees left, BinaryTrees right, int value) {
+        this.left = left;
+        this.right = right;
+        this.value = value;
+    }
+
+
+
+    public static void setRoot(BinaryTrees Node){
+        BinaryTrees.root = Node;
+        return;
+    }
+    public int getValue() {
+        return value;
+    }
+    public BinaryTrees getRight() {
+        return right;
+    }
+
+    public BinaryTrees getLeft() {
+        return left;
+    }
+
+
+    public static void insertNode(int key, BinaryTrees root){
+        if (root == null){
+            BinaryTrees Node = new BinaryTrees(null, null, key);
+            setRoot(Node);
+            return;
+        }
+        if (key < root.value && root.left == null){
+            BinaryTrees Node = new BinaryTrees(null, null, key);
+            root.left = Node;
+        }else if(key < root.value && root.left != null){
+            insertNode(key, root.left);
+        } else if (key > root.value && root.right != null){
+            insertNode(key, root.right);
+        }else{
+            BinaryTrees Node = new BinaryTrees(null, null, key);
+            root.right = Node;
+        }
+    }
+
+
+    public static int Predecessor(BinaryTrees Node){
+        if (Node == null){
+            return 0;
+        }
+        Node = Node.getLeft();
+        while (Node.getRight() != null){
+            Node = Node.getRight();
+        }
+        return Node.value;
+    }
+    public static int Successor(BinaryTrees Node){
+        if (Node == null){
+            return 0;
+        }
+        Node = Node.getRight();
+        while (Node.getLeft() != null){
+            Node = Node.getLeft();
+        }
+        return Node.value;
+    }
+
+
+    public static BinaryTrees deleteNode(int key, BinaryTrees Node){
+        if (Node == null){
+            return null;
+        }
+        if (key > Node.value){
+            Node.right = deleteNode(key, Node.right);
+        }else if(key < Node.value){
+            Node.left = deleteNode(key, Node.left);
+        }else{
+            if (Node.left == null && Node.right == null){
+                Node = null;
+            }else if (Node.right != null){
+                Node.value = Successor(Node);
+                Node.right = deleteNode(Node.value, Node.right);
+            }else if (Node.left != null){
+                Node.value = Predecessor(Node);
+                Node.left = deleteNode(Node.value, Node.left);
+            }
+        }
+        return Node;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        BinaryTrees Node = new BinaryTrees(null,null, 50);
+        BinaryTrees.setRoot(Node);
+        BinaryTrees.insertNode(228, Node);
+        BinaryTrees.insertNode(1337, Node);
+        BinaryTrees.insertNode(134, Node);
+        BinaryTrees.insertNode(777, Node);
+        BinaryTrees.insertNode(105, Node);
+        BinaryTrees.insertNode(808, Node);
+        BFS(Node);
+    }
+
+    public static void BFS(BinaryTrees Node){
+        Queue<BinaryTrees> Queue = new LinkedList<BinaryTrees>();
+
+        Queue.add(Node);
+        while (!Queue.isEmpty()){
+            BinaryTrees CNode = Queue.poll();
+            System.out.print(CNode.getValue() + " ");
+            if (CNode.getLeft() != null){
+                Queue.add(CNode.getLeft());
+            }
+            if (CNode.getRight() != null){
+                Queue.add(CNode.getRight());
+            }
+        }
     }
 }
